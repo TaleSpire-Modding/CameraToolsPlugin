786a498db5401e1d1d4c41059836ee56e8d78577
brajm008 <ujkkju117@outlook.com>
2022-01-29 01:19:35 +1030

    Add project files.

---
+++ CameraToolsPlugin/CameraToolsPlugin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace CameraToolsPlugin
+{
+
+    [BepInPlugin(Guid, "HolloFoxes' Camera Tools Plug-In", Version)]
+    public class CameraToolsPlugin : BaseUnityPlugin
+    {
+        // constants
+        public const string Guid = "org.hollofox.plugins.CameraToolsPlugin";
+        internal const string Version = "3.0.0";
+
+        // Configs
+        internal static ConfigEntry<float> minTilt { get; set; }
+        internal static ConfigEntry<float> maxTilt { get; set; }
+        /// <summary>
+        /// Awake plugin
+        /// </summary>
+        void Awake()
+        {
+            Logger.LogInfo("In Awake for Camera Tools");
+
+            minTilt = Config.Bind("Tilt Limit", "minimum", -124f);
+            maxTilt = Config.Bind("Tilt Limit", "maximum", 53f);
+
+            Debug.Log("CameraTools Plug-in loaded");
+
+            ModdingTales.ModdingUtils.Initialize(this, Logger);
+            var harmony = new Harmony(Guid);
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ CameraToolsPlugin/Patches/CameraControllerPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace CameraToolsPlugin.Patches
+{
+    [HarmonyPatch(typeof(RootTargetCameraMode), "Awake")]
+    internal class CameraControllerAwakePatch
+    {
+        internal static void Postfix(ref float ___maxTilt, ref float ___minTilt)
+        {
+            ___minTilt = CameraToolsPlugin.minTilt.Value;
+            ___maxTilt = CameraToolsPlugin.maxTilt.Value;
+        }
+    }
+}
